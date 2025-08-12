fe99d3b75dae1ac8a6116fa47be6e1675744fccd
Natanael Saymon <saymoneo@gmail.com>
2025-08-12 13:29:38 -0300

    feat: estruturação da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import styles from "./page.module.css";
+import styles from "../styles/page.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Página HOME</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.header__content}>header</div>
+      </header>
+
+      <section className={styles.banner}>
+        <div className={styles.banner__content}>
+          <img src="https://placehold.co/600x300?text=Hello+World" alt="" />
+        </div>
+      </section>
+
+      <main className={styles.main}>
+        <div className={styles.main__content}>main</div>
+      </main>
     </div>
   );
 }
