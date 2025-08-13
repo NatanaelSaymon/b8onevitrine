729810078abfb4302851cd791ae6c9d6dea6c0d4
Natanael Saymon <saymoneo@gmail.com>
2025-08-13 19:36:48 -0300

    feat: filtro de categoria

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -8,6 +8,8 @@ import styles from "./styles.module.scss";
 
 export default function ProductList() {
   const [product, setProduct] = useState<Product[]>([]);
+  //
+  const [selectedCategory, setSelectedCategory] = useState<string>("todos");
 
   const loadData = async () => {
     const data = await loadProducts();
@@ -18,17 +20,54 @@ export default function ProductList() {
     loadData();
   }, []);
 
+  // Pega categorias únicas e coloca "all" no começo
+  const categories = ["todos", ...new Set(product.map((p) => p.category))];
+
+  // Filtra os produtos pela categoria selecionada
+  const filteredProducts =
+    selectedCategory === "todos"
+      ? product
+      : product.filter((p) => p.category === selectedCategory);
+
   console.log(product);
   return (
+    // <div className={styles.productlist}>
+    //   <section className={styles.productlist__content}>
+    //     {product?.slice(0, 6).map((product) => (
+    //       <ProductCard
+    //         key={product?.id}
+    //         title={product?.title}
+    //         image={product?.image}
+    //         price={product?.price}
+    //         description={product?.description}
+    //       />
+    //     ))}
+    //   </section>
+    // </div>
+
     <div className={styles.productlist}>
+      {/* Botões de categoria */}
+      <div className={styles.categoryButtons}>
+        {categories.map((cat: any) => (
+          <button
+            key={cat}
+            onClick={() => setSelectedCategory(cat)}
+            className={selectedCategory === cat ? styles.activeButton : ""}
+          >
+            {cat.toUpperCase()}
+          </button>
+        ))}
+      </div>
+
+      {/* Lista de produtos */}
       <section className={styles.productlist__content}>
-        {product?.slice(0, 6).map((product) => (
+        {filteredProducts.slice(0, 6).map((product) => (
           <ProductCard
-            key={product?.id}
-            title={product?.title}
-            image={product?.image}
-            price={product?.price}
-            description={product?.description}
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+            description={product.description}
           />
         ))}
       </section>
