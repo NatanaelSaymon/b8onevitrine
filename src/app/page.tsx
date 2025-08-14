e92cea52b41ac231298ac72500b40588e4a04ac9
Natanael Saymon <saymoneo@gmail.com>
2025-08-14 16:16:39 -0300

    feat: ajustes mobile, cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className={styles.container}>
-      <section className={styles.banner}>
-        <div className={styles.banner__content}>
-          <img src="https://placehold.co/600x300?text=Hello+World" alt="" />
-        </div>
-      </section>
-
       <main className={styles.main}>
         <div className={styles.main__content}>main</div>
       </main>
